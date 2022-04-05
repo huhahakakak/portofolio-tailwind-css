fef5a3448bbc22b8a4ab58fac2b7aed428a0ff88
huhahakakak <huhahakakak@gmail.com>
2022-04-05 16:37:56 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#14b8a6',
+        dark: '#18181b',
+        semidark: '#71717a',
+      },
+      screens:{
+        '2xl' : '1320px',
+      },
+    },
+
+  },
+  plugins: [],
+}
